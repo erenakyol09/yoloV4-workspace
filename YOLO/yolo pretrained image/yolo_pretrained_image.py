092cff0e84b008b637fb41dbbad81646a294e8e5
Eren Akyol <erenakyol09@gmail.com>
2022-12-07 23:09:22 +0300

    mainde image detect yapildi.

--- YOLO/yolo pretrained image/yolo_pretrained_image.py
+++ YOLO/yolo pretrained image/yolo_pretrained_image.py
@@ -5,6 +5,10 @@ import numpy as np
 
 path = r'D:\Tekonofest\yoloV4-workspace\YOLO\yolo pretrained image\images\people.jpg'
 
+path2 = r'D:\Tekonofest\yoloV4-workspace\YOLO\pretrained_model\yolov3.cfg'
+
+path3 = r'D:\Tekonofest\yoloV4-workspace\YOLO\pretrained_model\yolov3.weights'
+
 img = cv2.imread(path)
 
 img_width = img.shape[1]
@@ -29,7 +33,50 @@ labels = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", 
           "remote", "keyboard", "cellphone", "microwave", "oven", "toaster", "sink", "refrigerator",
           "book", "clock", "vase", "scissors", "teddybear", "hairdrier", "toothbrush"]
 
+# kutucuk renkleri ayarlıyoruz :
+# buradaki kodları teker teker konsola yazınca değer oluşuyor
 colors = ["0,255,255", "0,0,255", "255,0,0", "255,255,0", "0,255,0"]
 colors = [np.array(color.split(",")).astype("int") for color in colors]
 colors = np.array(colors)
 colors = np.tile(colors, (18, 1))
+
+# modeli import ediyoruz : algoritma başlangıcı
+model = cv2.dnn.readNetFromDarknet(path2,path3)
+layers = model.getLayerNames()
+
+# çıktı katmanlarını araştırıyoruz
+output_layer = [layers[layer - 1] for layer in model.getUnconnectedOutLayers()]
+
+model.setInput(img_blob)
+
+# çıktı katmanlarını detectiona sokuyoruz
+detection_layers = model.forward(output_layer)
+# çıktı katmanlarının içindeki değerleri almış olduk.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
